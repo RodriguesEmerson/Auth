4a698f1d6e3fdc11c0689fb7190de065ce2ffd08
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-12-09 15:22:29 -0300

    Estudando o NextAuth

--- app/UI/loginBox.jsx
+++ app/UI/loginBox.jsx
@@ -4,17 +4,23 @@ import { Input } from "../components/input"
 import { SubmitButton } from "../components/submitButton"
 import { useEffect, useState } from "react"
 import { useScreenLoginStyleStore } from "../zustand/useScreenLoginStyleStore"
+import { useForm } from "react-hook-form"
 
 export const LoginBox = () => {
-
    const style = useScreenLoginStyleStore((state) => state.style);
    const setStyle = useScreenLoginStyleStore((state) => state.setStyle);
-
    const [boxInfos, setBoxInfos] = useState();
+   
    useEffect(() => {
       style === "login" && setBoxInfos({title: "Login Auth", actionLink: {title: "Criar conta", link: "#"}, buttonValue: "Login"});
       style === "create" && setBoxInfos({title: "Criar conta Auth", actionLink: {title: "Fazer login", link: "#"}, buttonValue:"Criar conta"});
-   },[style])
+   },[style]);
+   
+   const { register, handleSubmit } = useForm();
+   function handleSignIn(data){
+      console.log(data)
+   }
+
 
    return (
       <section
@@ -24,20 +30,24 @@ export const LoginBox = () => {
       >
          <h1 className="text-center absolute top-3 w-[95%]">{boxInfos?.title}</h1>
          <form
-            action=""
+            onSubmit={handleSubmit(handleSignIn)}
             className={`flex flex-col gap-2 mt-2 text-sm text-gray-600`}
          >
             <Input
                type="text"
                className={"h-10 w-full border border-gray-300 rounded-md px-2 outline-1 outline-blue-500"}
-               name="user"
+               name="email"
+               {...register('email')}
                placeholder="E-mail..."
+               resquired
             />
             <Input
                type="password"
                className={"h-10 w-full border border-gray-300 rounded-md px-2 outline-1 outline-blue-500"}
                name="password"
+               {...register('password')}
                placeholder="Senha..."
+               resquired
             />
             <SubmitButton
                className={`h-10 w-full outline-1 outline-blue-500  text-white rounded-md transition-all 
